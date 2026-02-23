366baa0a411478570c222dc6892c0fe81994f942
Ayfri <pierre.ayfri@gmail.com>
2026-02-23 21:07:48 +0100

    feat(layout): Add Footer component.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,7 @@
 	import '$lib/layout.css';
 	import favicon from '$lib/assets/favicon.svg';
 	import Header from '$lib/components/Header.svelte';
+	import Footer from '$lib/components/Footer.svelte';
 
 	let { children } = $props();
 </script>
@@ -23,4 +24,5 @@
 	<main class="flex-1 overflow-hidden">
 		{@render children()}
 	</main>
+	<Footer />
 </div>
