db86c49b9aa7c2348b8a5b303b97bb59dce65865
Ayfri <pierre.ayfri@gmail.com>
2026-02-24 14:40:31 +0100

    feat(layout): Enhance SEO metadata.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,14 +3,24 @@
 	import favicon from '$lib/assets/logo.png';
 	import Header from '$lib/components/Header.svelte';
 	import Footer from '$lib/components/Footer.svelte';
+	import { VERSION } from '@sveltejs/kit';
+	import type { Snippet } from 'svelte';
+	import type { LayoutServerData } from './$types.js';
 
-	let { children } = $props();
+	interface Props {
+		children: Snippet;
+		data: LayoutServerData;
+	}
+
+	const domain = 'https://shayders.ayfri.com';
+
+	let { children, data }: Props = $props();
 </script>
 
 <svelte:head>
-	<meta charset="UTF-8" />
-	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<meta name="generator" content="SvelteKit {VERSION}" />
 	<meta name="theme-color" content="#1e1e1e" />
+	<link rel="canonical" href={domain + data.pathname} />
 	<link rel="icon" type="image/png" href={favicon} />
 	<link rel="apple-touch-icon" sizes="400x400" href={favicon} />
 </svelte:head>
